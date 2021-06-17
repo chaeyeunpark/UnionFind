5d188c6e9646d227e7d2fb042316b6c46a1e1ae7
Chae-Yeun Park <kaeri17@gmail.com>
2021-06-17 20:47:55 +0200

    doc updated

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('_ext'))
 
 
 # -- Project information -----------------------------------------------------
@@ -44,9 +45,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "chaeyeunpark", # Username
+    "github_repo": "UnionFind", # Repo name
+    "github_version": "main", # Version
+    "conf_py_path": "/docs/", # Path in the checkout to the docs root
+}
--- docs/custom_lattice.rst
+++ docs/custom_lattice.rst
@@ -8,19 +8,19 @@ A custom lattice class should implement 5 methods.
 
 ::
 
-	class CustomLattice
-	{
-	public:
-		using Vertex = int;
+    class CustomLattice
+    {
+    public:
+        using Vertex = int;
 
-		int num_vertices(); //return number of all vertices in the lattice
-		int num_edges(); //return number of all edges (qubits) in the lattice
-	
-		std::vector<int> vertex_connections(Vertex v);  //return nearest neighbor vertices
-	
-		int edge_idx(Edge edge); //return index of edge for a given edge
-		int vertex_connection_count(Vertex v); //return the number of nearest neighbor vertices
-	};
+        int num_vertices(); //return number of all vertices in the lattice
+        int num_edges(); //return number of all edges (qubits) in the lattice
+
+        std::vector<int> vertex_connections(Vertex v);  //return nearest neighbor vertices
+
+        int edge_idx(Edge edge); //return index of edge for a given edge
+        int vertex_connection_count(Vertex v); //return the number of nearest neighbor vertices
+    };
 
 Then you can use our ``UnionFind`` template class in your C++ code as
 
