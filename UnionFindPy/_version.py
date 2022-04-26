5290a23eade68438b6083e69c9cdc3b2211228a6
Chae-Yeun Park <chae-yeun@Xanadu.ai>
2022-04-26 14:56:19 -0400

    Remove license comments

--- UnionFindPy/_version.py
+++ UnionFindPy/_version.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with UnionFind++.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "0.2.1"
+__version__ = "0.3.0-dev"
